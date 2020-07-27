24e39fe33b8ae0ce8007958f67213d5e3cf17993
Piklu Dey <piklu@rewardz.sg>
2020-07-27 07:43:37 +0530

    flag post table added

---
+++ feeds/migrations/0009_flagpost.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('feeds', '0008_auto_20200716_0701'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FlagPost',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('notes', models.TextField(verbose_name=b'reason')),
+                ('accepted', models.BooleanField(default=True)),
+                ('created_on', models.DateTimeField(auto_now_add=True)),
+                ('modified_on', models.DateTimeField(auto_now=True)),
+                ('flagger', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('post', models.ForeignKey(to='feeds.Post')),
+            ],
+        ),
+    ]
