b03f8492ddbffc89abc8fe99325d14415ff84ee2
Chip <thechip911@gmail.com>
2022-01-27 18:41:42 +0530

    Minor Fix

--- feeds/views.py
+++ feeds/views.py
@@ -287,7 +287,7 @@ class PostViewSet(viewsets.ModelViewSet):
         liked = False
         message = "Successfully unliked"
         post_object = None
-        if PostLiked.gsobjects.filter(post_id=post_id, created_by=user).exists():
+        if PostLiked.objects.filter(post_id=post_id, created_by=user).exists():
             if not reaction_type:
                 PostLiked.objects.filter(post_id=post_id, created_by=user).delete()
             else:
