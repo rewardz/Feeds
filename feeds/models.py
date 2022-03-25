66f32c8e42a8eee0c14d2964fa2709efd40f2e0a
actionAxolot <mario.r.vallejo@gmail.com>
2022-03-24 22:52:25 -0600

    Dependencies compatible with django4 and removal of reference to ugettext

--- feeds/models.py
+++ feeds/models.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.db.models import Sum
 from django.utils import timezone
 from django.utils.module_loading import import_string
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from rest_framework.exceptions import ValidationError
 
