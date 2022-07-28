740851e2ebf78ae7da24414d969a8f32dfc2ed6f
yogendrakatewa <yogendra.katewa@trootech.com>
2022-07-28 13:58:30 +0530

    fixed migratiion issue

--- feeds/migrations/0016_auto_20220728_0827.py
+++ feeds/migrations/0016_auto_20220728_0827.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('feeds', '0016_ecard_name'),
+        ('feeds', '0015_auto_20220707_0924'),
     ]
 
     operations = [
@@ -15,6 +15,11 @@ class Migration(migrations.Migration):
             model_name='commentliked',
             name='reaction_type',
             field=models.SmallIntegerField(default=0, choices=[(6, 'Applause'), (1, 'Celebrate'), (5, 'Curious'), (4, 'Insightful'), (0, 'Like'), (3, 'Love'), (2, 'Support')]),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='post_type',
+            field=models.SmallIntegerField(default=1, choices=[(5, 'Most appreciated'), (4, 'Most liked'), (3, 'System created post'), (6, 'User created appreciation'), (7, 'User created nomination'), (2, 'User created poll'), (1, 'User created post')]),
         ),
         migrations.AlterField(
             model_name='postliked',
