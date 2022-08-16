3edddbd4b99eb067b5ee86c551bbe84418eaae25
prashant-rewardz <prashant@rewardz.sg>
2022-08-16 17:37:51 +0530

    fix-user-feed

--- feeds/views.py
+++ feeds/views.py
@@ -862,7 +862,7 @@ class UserFeedViewSet(viewsets.ModelViewSet):
         show_cheer_msg = False
         feeds = Post.objects.filter(Q(post_type=POST_TYPE.USER_CREATED_APPRECIATION) |
                                     Q(nomination__nom_status=NOMINATION_STATUS.approved),
-                                    created_by=request.user).distinct()
+                                    user=request.user).distinct()
         # returns latest 5 appreciations from last 30 days
         start_date, end_date = get_date_range(30)
         feeds = feeds.filter(created_on__gte=start_date, created_on__lte=end_date)[:5]
