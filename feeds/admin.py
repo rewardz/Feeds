060af8a473de64ef77f1b938c7bc8d39a840cf42
prashant-rewardz <71642736+prashant-rewardz@users.noreply.github.com>
2022-12-08 17:20:59 +0530

    filter-by-organization (#105)

--- feeds/admin.py
+++ feeds/admin.py
@@ -18,7 +18,7 @@ class PostAdmin(admin.ModelAdmin):
         'created_by', 'created_on', 'modified_on', 'modified_by', 'mark_delete',
     )
     readonly_fields = ('transaction', 'nomination', 'cc_users', 'created_by', 'modified_by',)
-    list_filter = ('priority', 'mark_delete',)
+    list_filter = ('priority', 'mark_delete', 'organizations')
 
     formfield_overrides = {
         # Make many to many field user FilteredSelectMultiple widget instead of the default
