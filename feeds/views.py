db43cc17a20f9a5562a3136fb20ee7c2570ca071
Yatish prajapati <yatish@rewardz.sg>
2023-03-16 18:57:06 +0530

    Fixed allowed feedback paramaeter

--- feeds/views.py
+++ feeds/views.py
@@ -1076,7 +1076,7 @@ class UserFeedViewSet(viewsets.ModelViewSet):
         user = self.request.user
         post_polls = request.query_params.get("post_polls", None)
         organizations = user.organization
-        posts = accessible_posts_by_user(user, organizations, False if post_polls else True)
+        posts = accessible_posts_by_user(user, organizations, False, False if post_polls else True)
 
         if post_polls:
             feeds = posts.filter((Q(post_type=POST_TYPE.USER_CREATED_POST) |
