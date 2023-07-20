4d81e7d5b60487c26043c47fd38b1a766116b6a7
Yatish prajapati <114574834+yatishTrootech@users.noreply.github.com>
2023-07-20 09:45:56 +0530

    Leaderboard changes (#158)
    * Custom User id is allowed in the API to list user specific given and received app
    
    * flake fix

--- feeds/views.py
+++ feeds/views.py
@@ -947,7 +947,14 @@ class UserFeedViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         feed_flag = self.request.query_params.get("feed", None)
         search = self.request.query_params.get("search", None)
+        user_id = self.request.query_params.get("user_id", None)
         user = self.request.user
+        if user_id and str(user_id).isdigit() and feed_flag in ("received", "given"):
+            try:
+                user = CustomUser.objects.get(id=user_id)
+            except CustomUser.DoesNotExist:
+                raise ValidationError("Invalid user id")
+
         organization = user.organization
         posts = accessible_posts_by_user(user, organization, False, feed_flag != "post_polls")
         if feed_flag == "post_polls":
