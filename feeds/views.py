6b313c2c1a9976e50b5bea06b3660920f051ef78
Yoseph Parai <yosephoche@gmail.com>
2023-09-20 13:58:02 +0800

    add all inspire me endpoint

--- feeds/views.py
+++ feeds/views.py
@@ -1266,3 +1266,27 @@ class InspireMeViewSet(viewsets.ModelViewSet):
         response = InspireMe.amplify_core_value_recognition(request.data)
 
         return Response(response, status=status.HTTP_200_OK)
+
+    @list_route(methods=["POST"], permission_classes=(IsOptionsOrAuthenticated,))
+    def edit_tone(self, request, *args, **kwargs):
+        response = InspireMe.edit_tone(request.data)
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    @list_route(methods=["POST"], permission_classes=(IsOptionsOrAuthenticated,))
+    def amplify_content_post(self, request, *args, **kwargs):
+        response = InspireMe.amplify_content_post(request.data)
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    @list_route(methods=["POST"], permission_classes=(IsOptionsOrAuthenticated,))
+    def amplify_content_poll(self, request, *args, **kwargs):
+        response = InspireMe.amplify_content_poll(request.data)
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    @list_route(methods=["POST"], permission_classes=(IsOptionsOrAuthenticated,))
+    def proof_read_content(self, request, *args, **kwargs):
+        response = InspireMe.proof_read_content(request.data)
+
+        return Response(response, status=status.HTTP_200_OK)
