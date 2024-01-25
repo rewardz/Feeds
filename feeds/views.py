182d65ac14cbded64473ecf8fe21e8a1b5044e02
prashant <prashant@rewardz.sg>
2024-01-25 13:19:39 +0530

    Allow like for others comment

--- feeds/views.py
+++ feeds/views.py
@@ -839,7 +839,7 @@ class CommentViewset(viewsets.ModelViewSet):
         posts = accessible_posts_by_user(user, user.organization, False,
                                          is_appreciation_post(post_id) if post_id else False)
 
-        result = Comment.objects.filter(post__in=posts, mark_delete=False, created_by=user)
+        result = Comment.objects.filter(post__in=posts, mark_delete=False)
         return result
 
     @detail_route(methods=["POST"], permission_classes=(IsOptionsOrAuthenticated,))
