012417980b7cd3440a8a656952362ec3e4025049
Yatish prajapati <yatish@rewardz.sg>
2024-04-18 18:57:27 +0530

    Migrate celery command

---
+++ feeds/management/commands/migrate_feed_users.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from feeds.models import Post
+from django.core.management.base import BaseCommand
+from rewardz_utils.processmonitor.decorators import write_process_monitor_logs
+
+
+class Command(BaseCommand):
+    help = """Used to migrate Post[user] to Post[users] model"""
+
+    @write_process_monitor_logs(site="skor", command="migrate_feed_users")
+    def handle(self, *args, **options):
+        posts = Post.objects.all()
+        for post in posts.iterator():
+            try:
+                user = post.user
+                if user is not None and not post.users.all():
+                    post.users.add(user)
+                    if post.user.all().first() != user:
+                        raise Exception("Invalid user migration {}".format(post.id))
+            except Exception as e:
+                print("Exception {}".format(str(e)))
+                continue
