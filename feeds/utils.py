a58f36a41f3ed740bb16a50c7e2e2119bafd1e7f
rishireward <rishi@rewardz.sg>
2024-06-12 15:30:34 +0530

    Handle if job_family not present

--- feeds/utils.py
+++ feeds/utils.py
@@ -291,11 +291,12 @@ def fetch_feeds(post_query, limited_date_query, exclusion_query, page_size, orde
         queryset = Post.objects.filter(post_query).exclude(exclusion_query)
 
     post_ids = set(queryset.values_list('id', flat=True))
-    filter_params = {
-        'job_families__in': [user.job_family.id],
-    }
-    queryset = Post.objects.filter(id__in=post_ids)
-    queryset = PostJobFamilyFilter(filter_params, queryset=queryset).queryset
+    if getattr(user, 'job_family', None):
+        filter_params = {
+            'job_families__in': [user.job_family.id],
+        }
+        queryset = Post.objects.filter(id__in=post_ids)
+        queryset = PostJobFamilyFilter(filter_params, queryset=queryset).queryset
     return get_related_objects_qs(
         queryset.order_by(*ordering_fields)
     )
