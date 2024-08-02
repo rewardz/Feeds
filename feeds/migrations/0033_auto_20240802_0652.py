ad08498f561cd92e98508e47d57299c65cf68280
Govind Sharma <govindsharma@192.168.1.6>
2024-08-02 11:23:22 +0530

    fix: migrations

---
+++ feeds/migrations/0033_auto_20240802_0652.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feeds', '0032_auto_20240611_0957'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='source_language',
+            field=models.CharField(max_length=100, null=True, blank=True),
+        ),
+    ]
